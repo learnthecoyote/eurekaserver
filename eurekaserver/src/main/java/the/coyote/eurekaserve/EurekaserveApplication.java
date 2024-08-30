a984d307b1f137a7db4cbefcca97d95ab330de3a
theCoyote <flaviolunaferreira@gmail.com>
2024-08-30 08:42:52 -0300

    Add files via upload

---
+++ eurekaserver/src/main/java/the/coyote/eurekaserve/EurekaserveApplication.java
@@ -0,0 +1,15 @@
+package the.coyote.eurekaserve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaserveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaserveApplication.class, args);
+	}
+
+}
---
+++ eurekaserver/src/test/java/the/coyote/eurekaserve/EurekaserveApplicationTests.java
@@ -0,0 +1,13 @@
+package the.coyote.eurekaserve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
